f94c38750b703b28189c40949d3e6b2a0a5e32fc
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-06-11 15:56:34 +0200

    Project layout

---
+++ Chess.Game/Chess.Game.Domain/Board.cs
@@ -0,0 +1,30 @@
+﻿namespace Chess.Game.Domain
+{
+    public class Board
+    {
+        private readonly List<Piece> _pieces = new List<Piece>
+        {
+            new Piece("Pawn","White","A",2)
+        };
+        public void Move(string startColumn, int startRow, string endColumn, int endRow)
+        {
+            if(TryFindPiece(startColumn, startRow, out Piece piece))
+            {
+                _pieces.Remove(piece);
+                _pieces.Add(piece.Move(endColumn, endRow));
+                
+            }
+        }
+
+        private bool TryFindPiece(string startColumn, int startRow, out Piece piece)
+        {
+            if(!_pieces.Any(p=>p.Column == startColumn && p.Row == startRow))
+            {
+                piece = default(Piece);
+                return false;
+            }
+            piece = _pieces.First(p => p.Column == startColumn && p.Row == startRow);
+            return false;
+        }
+    }
+}
---
+++ Chess.Game/Chess.Game.Domain/Piece.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess.Game.Domain
+{
+    public record Piece(string Type, string Color, string Column, int Row)
+    {
+        public Piece Move(string column, int row)
+        {
+            return new Piece(Type, Color, column, row);
+        }
+    }
+}
